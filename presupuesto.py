98490941c5d0a6bb2b70ca466cd9c2170a56dfbd
Paolam1903 <sernapaola922@gmail.com>
2026-05-28 13:42:06 -0500

    Add files via upload

--- presupuesto.py
+++ presupuesto.py
@@ -66,21 +66,21 @@ def calcular_distribucion(n_asesores, cvs, nombre=None, rol=None):
     if cvs == "ITAGUI":
 
         # TOTAL META = 2500
-        # Marcela = 781
-        # Dailyn = 1172
-        # Jhon = 547
+        # Marcela = 797
+        # Dailyn = 1195
+        # Jhon = 508
 
         # 👩 Marcela
         if "MARCELA" in nombre:
-            return 781 / 2500
+            return 797 / 2500
 
         # 👩 Dailyn
         elif "DAILYN" in nombre:
-            return 1172 / 2500
+            return 1195 / 2500
 
         # 👨 Jhon
         elif "JHON" in nombre:
-            return 547 / 2500
+            return 508 / 2500
 
     # ==================================================
     # 🔴 REGLA ESPECIAL FRONTINO
